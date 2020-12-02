b442b72b1338b1be945e274915ef3fda0a7488d0
Matt Llanes <llanesm@users.noreply.github.com>
2020-12-02 06:54:42 -0700

    debug verify

--- sudoku.py
+++ sudoku.py
@@ -2,6 +2,8 @@
 # Date: 12/1/2020
 # Description: Playable Sudoku game, functions for solving the game
 
+import math
+
 sudoku_game_instance = [[3, 0, 0, 1, 4, 0, 2, 0, 5],
                         [0, 0, 1, 0, 9, 8, 0, 0, 4],
                         [6, 0, 0, 2, 7, 0, 0, 0, 0],
@@ -22,18 +24,19 @@ def valid_move(grid,x,y,num):
     :return: True if number is possible, false otherwise
     """
     # check if in-line spots contain num
-    for i in range(0, 9):
-        if grid[y][i] == num:   # across
+    for i in range(0, len(grid)):
+        if grid[y][i] == num and i != x:   # across
             return False
-        if grid[i][x] == num:   # up or down
+        if grid[i][x] == num and i != y:   # up or down
             return False
 
     # check if 3*3 section contains num
-    sec_x = (x // 3) * 3    # leftmost index of section
-    sec_y = (y // 3) * 3    # topmost index of section
-    for i in range(0, 3): # denotes y of section
-        for j in range(0, 3):   # denotes x of section
-            if grid[sec_y + i][sec_x + j] == num:   # check at each square within section
+    sec_size = int(math.sqrt(len(grid)))
+    sec_x = (x // sec_size) * sec_size    # leftmost index of section
+    sec_y = (y // sec_size) * sec_size    # topmost index of section
+    for i in range(0, sec_size): # denotes y of section
+        for j in range(0, sec_size):   # denotes x of section
+            if grid[sec_y + i][sec_x + j] == num and sec_y + i != y and sec_x + j != x:   # check at each square within section
                 return False
 
     return True # made it through every condition, this is indeed a valid move
@@ -45,8 +48,8 @@ def verify_solution(grid):
     :param grid: player given solution to a sudoku grid
     :return: True if valid solution, False otherwise
     """
-    for y in range(0, 9):   # traverse lists
-        for x in range(0, 9):   # traverse values in lists
+    for y in range(0, len(grid)):   # traverse lists
+        for x in range(0, len(grid)):   # traverse values in lists
             num = grid[y][x]
             if not valid_move(grid, x, y, num): # checks if move is valid
                 return False
@@ -59,17 +62,17 @@ def find_solution(grid):
     :param grid: unsolved sudoku grid
     :return: True if solution exists, false otherwise
     """
-    for y in range(0, 9):
-        for x in range(0, 9):
-            if grid[y][x] == 0:
-                for num in range(1, 10):
-                    if valid_move(grid, x, y, num):
-                        grid[y][x] = num
-                        find_solution(grid)
-                        if is_full(grid) == True:
-                            return
-                        grid[y][x] = 0
-                return
+    for y in range(0, len(grid)):
+        for x in range(0, len(grid)):
+            if grid[y][x] == 0:	# is empty
+                for num in range(1, 10):	# usable numbers
+                    if valid_move(grid, x, y, num):	# if valid move is true
+                        grid[y][x] = num	# insert number into square
+                        find_solution(grid)	# recursive call to find next insert
+                        if is_full(grid) == True:	# grid is full, we're done'
+                            return True
+                        grid[y][x] = 0	# backtrack when a value no longer works, makes space empty
+                return False	# no solution
 
 
 def is_full(grid):
@@ -78,14 +81,19 @@ def is_full(grid):
     :param grid: sudoku grid to check
     :return: True if full, False otherwise
     """
-    for y in range(0, 9):
-        for x in range(0, 9):
+    for y in range(0, len(grid)):
+        for x in range(0, len(grid)):
             if grid[y][x] == 0:
                 return False
     return True
 
 
-print(find_solution(sudoku_game_instance))
-for i in range(0, 9):
-    print(sudoku_game_instance[i])
-print(verify_solution(sudoku_game_instance))
+def main():
+	find_solution(sudoku_game_instance)
+	for i in range(0, len(sudoku_game_instance)):
+		print(sudoku_game_instance[i])
+	print(verify_solution(sudoku_game_instance))
+	
+	
+if __name__ == '__main__':
+	main()
